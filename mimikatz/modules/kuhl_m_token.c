f0bfb0da31535b5b0b37addbfe0ef9ae733970ff
Benjamin DELPY <benjamin@gentilkiwi.com>
2017-03-28 04:22:57 +0300

    [enhancement] token: less listed (NtCompareTokens), more details in whoami

--- mimikatz/modules/kuhl_m_token.c
+++ mimikatz/modules/kuhl_m_token.c
@@ -24,7 +24,7 @@ NTSTATUS kuhl_m_token_whoami(int argc, wchar_t * argv[])
 	kprintf(L" * Process Token : ");
 	if(OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
 	{
-		kuhl_m_token_displayAccount(hToken);
+		kuhl_m_token_displayAccount(hToken, argc);
 		CloseHandle(hToken);
 	}
 	else PRINT_ERROR_AUTO(L"OpenProcessToken");
@@ -32,7 +32,7 @@ NTSTATUS kuhl_m_token_whoami(int argc, wchar_t * argv[])
 	kprintf(L" * Thread Token  : ");
 	if(OpenThreadToken(GetCurrentThread(), TOKEN_QUERY, TRUE, &hToken))
 	{
-		kuhl_m_token_displayAccount(hToken);
+		kuhl_m_token_displayAccount(hToken, argc);
 		CloseHandle(hToken);
 	}
 	else if(GetLastError() == ERROR_NO_TOKEN)
@@ -117,7 +117,8 @@ NTSTATUS kuhl_m_token_list_or_elevate(int argc, wchar_t * argv[], BOOL elevate, 
 		kprintf(L"\n");
 		
 		if(!elevate || !runIt || pData.tokenId || pData.pSid || pData.pUsername)
-			kull_m_token_getTokens(kuhl_m_token_list_or_elevate_callback, &pData);
+			kull_m_token_getTokensUnique(kuhl_m_token_list_or_elevate_callback, &pData);
+			//kull_m_token_getTokens(kuhl_m_token_list_or_elevate_callback, &pData);
 		
 		if(pData.pSid)
 			LocalFree(pData.pSid);
@@ -131,21 +132,58 @@ NTSTATUS kuhl_m_token_revert(int argc, wchar_t * argv[])
 {
 	if(SetThreadToken(NULL, NULL))
 		kuhl_m_token_whoami(0, NULL);
-	else
-		PRINT_ERROR_AUTO(L"SetThreadToken");
+	else PRINT_ERROR_AUTO(L"SetThreadToken");
 	return STATUS_SUCCESS;
 }
 
+void kuhl_m_token_displayAccount_sids(UCHAR l, DWORD count, PSID_AND_ATTRIBUTES sids)
+{
+	DWORD i;
+	PWSTR name, domainName;
+	for(i = 0; i < count; i++)
+	{
+		if(kull_m_token_getNameDomainFromSID(sids[i].Sid, &name, &domainName, NULL, NULL))
+		{
+			kprintf(L"   %c:[%c%c%c%c%c%c%c] ", l,
+				(sids[i].Attributes & SE_GROUP_MANDATORY) ? L'M' : L' ',
+				(sids[i].Attributes & SE_GROUP_ENABLED_BY_DEFAULT) ? L'D' : L' ',
+				(sids[i].Attributes & SE_GROUP_ENABLED) ? L'E' : L' ',
+				(sids[i].Attributes & SE_GROUP_OWNER) ? L'O' : L' ',
+				(sids[i].Attributes & SE_GROUP_USE_FOR_DENY_ONLY) ? L'U' : L' ',
+				(sids[i].Attributes & SE_GROUP_LOGON_ID) ? L'L' : L' ',
+				(sids[i].Attributes & SE_GROUP_RESOURCE) ? L'R' : L' '
+				);
+			if(lstrlen(domainName))
+				kprintf(L"%s\\", domainName);
+			kprintf(L"%s\n", name);
+			LocalFree(name);
+			LocalFree(domainName);
+		}
+	}
+
+}
 const wchar_t * KUHL_M_TOKEN_IMPERSONATION_LEVEL[] = {L"Anonymous", L"Identification", L"Impersonation", L"Delegation",};
 const wchar_t * KUHL_M_TOKEN_TYPE[] = {L"Unknown", L"Primary", L"Impersonation",};
-void kuhl_m_token_displayAccount(HANDLE hToken)
+void kuhl_m_token_displayAccount(HANDLE hToken, BOOL full)
 {
-	ULONG szNeeded;
 	TOKEN_STATISTICS tokenStats;
 	PWSTR name, domainName, sid;
-	
+	TOKEN_ELEVATION_TYPE type;
+	DWORD i, szNeeded;
+	PTOKEN_GROUPS_AND_PRIVILEGES p;
+
 	if(GetTokenInformation(hToken, TokenStatistics, &tokenStats, sizeof(TOKEN_STATISTICS), &szNeeded))
 	{
+		kprintf(L"{%x;%08x} ", tokenStats.AuthenticationId.HighPart, tokenStats.AuthenticationId.LowPart);
+		if(GetTokenInformation(hToken, TokenSessionId, &i, sizeof(DWORD), &szNeeded))
+			kprintf(L"%u ", i);
+		else kprintf(L"- ");
+
+		if(GetTokenInformation(hToken, TokenElevationType, &type, sizeof(TOKEN_ELEVATION_TYPE), &szNeeded))
+			kprintf(L"%c ", ((type == TokenElevationTypeDefault) ? L'D' : ((type == TokenElevationTypeFull) ? L'F' : (type == TokenElevationTypeLimited) ? L'L' : L'?')));
+		else kprintf(L"- ");
+
+
 		kprintf(L"%-10u\t", tokenStats.TokenId.LowPart);
 		if(kull_m_token_getNameDomainFromToken(hToken, &name, &domainName, &sid, NULL))
 		{
@@ -158,6 +196,44 @@ void kuhl_m_token_displayAccount(HANDLE hToken)
 		if(tokenStats.TokenType == TokenImpersonation)
 			kprintf(L" (%s)", KUHL_M_TOKEN_IMPERSONATION_LEVEL[tokenStats.ImpersonationLevel]);
 		kprintf(L"\n");
+
+		if(full)
+		{
+			if(!GetTokenInformation(hToken, TokenGroupsAndPrivileges, NULL, 0, &szNeeded) && (GetLastError() == ERROR_INSUFFICIENT_BUFFER))
+			{
+				if(p = (PTOKEN_GROUPS_AND_PRIVILEGES) LocalAlloc(LPTR, szNeeded))
+				{
+					if(GetTokenInformation(hToken, TokenGroupsAndPrivileges, p, szNeeded, &szNeeded))
+					{
+						kuhl_m_token_displayAccount_sids(L'G', p->SidCount - 1, p->Sids + 1);
+						kuhl_m_token_displayAccount_sids(L'R', p->RestrictedSidCount, p->RestrictedSids);
+						for(i = 0; i < p->PrivilegeCount; i++)
+						{
+							kprintf(L"   P:[%c%c%c%c]    ",
+								(p->Privileges[i].Attributes & SE_PRIVILEGE_ENABLED_BY_DEFAULT) ? L'D' : L' ',
+								(p->Privileges[i].Attributes & SE_PRIVILEGE_ENABLED) ? L'E' : L' ',
+								(p->Privileges[i].Attributes & SE_PRIVILEGE_REMOVED) ? L'R' : L' ',
+								(p->Privileges[i].Attributes & SE_PRIVILEGE_USED_FOR_ACCESS) ? L'A' : L' '
+								);
+							szNeeded = 0;
+							if(!LookupPrivilegeName(NULL, &p->Privileges[i].Luid, NULL, &szNeeded) && (GetLastError() == ERROR_INSUFFICIENT_BUFFER))
+							{
+								if(name = (PWSTR) LocalAlloc(LPTR, (szNeeded + 1) * sizeof(wchar_t)))
+								{
+									if(LookupPrivilegeName(NULL, &p->Privileges[i].Luid, name, &szNeeded))
+										kprintf(L"%s\n", name);
+									LocalFree(name);
+								}
+							}
+							else if(GetLastError() == ERROR_NO_SUCH_PRIVILEGE)
+								kprintf(L"{%x; %08x}\n", p->Privileges[i].Luid.HighPart, p->Privileges[i].Luid.LowPart);
+							else PRINT_ERROR_AUTO(L"LookupPrivilegeName");
+						}
+					}
+					LocalFree(p);
+				}
+			}
+		}
 	}
 }
 
@@ -196,7 +272,7 @@ BOOL CALLBACK kuhl_m_token_list_or_elevate_callback(HANDLE hToken, DWORD ptid, P
 			if(isUserOK)
 			{
 				kprintf(L"%u\t", ptid);
-				kuhl_m_token_displayAccount(hToken);
+				kuhl_m_token_displayAccount(hToken, FALSE);
 				if(pData->elevateIt)
 				{
 					ttTarget = TokenImpersonation;
--- mimikatz/modules/kuhl_m_token.h
+++ mimikatz/modules/kuhl_m_token.h
@@ -33,7 +33,8 @@ typedef struct _KUHL_M_TOKEN_ELEVATE_DATA {
 	//BOOL isMinimal;
 } KUHL_M_TOKEN_ELEVATE_DATA, *PKUHL_M_TOKEN_ELEVATE_DATA;
 
-void kuhl_m_token_displayAccount(HANDLE hToken);
+void kuhl_m_token_displayAccount_sids(UCHAR l, DWORD count, PSID_AND_ATTRIBUTES sids);
+void kuhl_m_token_displayAccount(HANDLE hToken, BOOL full);
 
 NTSTATUS kuhl_m_token_whoami(int argc, wchar_t * argv[]);
 NTSTATUS kuhl_m_token_list(int argc, wchar_t * argv[]);
