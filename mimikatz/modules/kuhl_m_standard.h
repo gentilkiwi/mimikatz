083e528b69285d19f26f1a1411d8c0a73527c865
Benjamin DELPY <benjamin@gentilkiwi.com>
2017-06-18 18:45:55 +0200

    Few code lines added... [fix] mimikatz sekurlsa::* for Windows 2003 older versions. [new] mimikatz version try to detect Credential Guard and display files version with arg.

--- mimikatz/modules/kuhl_m_standard.h
+++ mimikatz/modules/kuhl_m_standard.h
@@ -7,6 +7,7 @@
 #include "kuhl_m.h"
 #include "../modules/kull_m_string.h"
 #include "../modules/kull_m_file.h"
+#include "../modules/kull_m_process.h"
 
 const KUHL_M kuhl_m_standard;
 
--- mimikatz/modules/sekurlsa/crypto/kuhl_m_sekurlsa_nt5.h
+++ mimikatz/modules/sekurlsa/crypto/kuhl_m_sekurlsa_nt5.h
@@ -6,12 +6,11 @@
 #pragma once
 #include "../globals_sekurlsa.h"
 
-#define KUHL_M_SEKURLSA_NT5_SP2_MIN_TIMESTAMP	0x45d70a62
-
 NTSTATUS kuhl_m_sekurlsa_nt5_init();
 NTSTATUS kuhl_m_sekurlsa_nt5_clean();
 PLSA_PROTECT_MEMORY kuhl_m_sekurlsa_nt5_pLsaProtectMemory, kuhl_m_sekurlsa_nt5_pLsaUnprotectMemory;
 
+BOOL kuhl_m_sekurlsa_nt5_isOld(DWORD osBuildNumber, DWORD moduleTimeStamp);
 NTSTATUS kuhl_m_sekurlsa_nt5_acquireKeys(PKUHL_M_SEKURLSA_CONTEXT cLsass, PKULL_M_PROCESS_VERY_BASIC_MODULE_INFORMATION lsassLsaSrvModule);
 BOOL kuhl_m_sekurlsa_nt5_acquireKey(PKULL_M_MEMORY_ADDRESS aLsassMemory, PBYTE Key, SIZE_T taille);
 
