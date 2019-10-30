f234e18279fb58df79e4620bd7ee6e8bd7d0be0f
Francesco Soncina <phra@users.noreply.github.com>
2019-10-30 14:43:32 +0100

    Parse /base64 option of log command

--- mimikatz/modules/kuhl_m_standard.c
+++ mimikatz/modules/kuhl_m_standard.c
@@ -72,6 +72,7 @@ NTSTATUS kuhl_m_standard_sleep(int argc, wchar_t * argv[])
 NTSTATUS kuhl_m_standard_log(int argc, wchar_t * argv[])
 {
 	PCWCHAR filename = (kull_m_string_args_byName(argc, argv, L"stop", NULL, NULL) ? NULL : (argc ? argv[0] : MIMIKATZ_DEFAULT_LOG));
+	kull_m_string_args_bool_byName(argc, argv, L"base64", &isBase64Output);
 	kprintf(L"Using \'%s\' for logfile : %s\n", filename, kull_m_output_file(filename) ? L"OK" : L"KO");
 	return STATUS_SUCCESS;
 }
