cf8f9f3ee481f9e0461c81003ec486fa33dcc4d5
Benjamin DELPY <benjamin@gentilkiwi.com>
2020-07-15 16:36:17 +0200

    [typo] mimilib knp nIndex

--- mimilib/knp.h
+++ mimilib/knp.h
@@ -8,4 +8,4 @@
 #include <npapi.h>
 
 DWORD WINAPI knp_NPLogonNotify(PLUID lpLogonId, LPCWSTR lpAuthentInfoType, LPVOID lpAuthentInfo, LPCWSTR lpPreviousAuthentInfoType, LPVOID lpPreviousAuthentInfo, LPWSTR lpStationName, LPVOID StationHandle, LPWSTR *lpLogonScript);
-DWORD WINAPI knp_NPGetCaps(DWORD ndex);
+DWORD WINAPI knp_NPGetCaps(DWORD nIndex);
