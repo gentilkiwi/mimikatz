b66555c74b0c3d676d51de6bb9a7cf8b88e55524
Josh Terrill <joshterrill.dev@gmail.com>
2022-10-07 11:00:37 -0700

    added ldap_memfree calls after usages of ldap_get_dn

--- mimikatz/modules/lsadump/kuhl_m_lsadump_dc.c
+++ mimikatz/modules/lsadump/kuhl_m_lsadump_dc.c
@@ -209,6 +209,7 @@ BOOL kuhl_m_lsadump_dcsync_SearchAndParseLDAPToIntId(PLDAP ld, PWCHAR dn, PWCHAR
 	PLDAPMessage pMessage = NULL, pEntry;
 	PBERVAL *pId;
 	PSTR tmpString;
+	PWSTR tmpLdapDn;
 
 	ret = ldap_search_s(ld, dn, LDAP_SCOPE_ONELEVEL, req, myAttrs, FALSE, &pMessage);
 	if(ret == LDAP_SUCCESS)
@@ -217,7 +218,9 @@ BOOL kuhl_m_lsadump_dcsync_SearchAndParseLDAPToIntId(PLDAP ld, PWCHAR dn, PWCHAR
 		{
 			if(pEntry = ldap_first_entry(ld, pMessage))
 			{
-				kprintf(L"[ldap] %s : ", ldap_get_dn(ld, pEntry));
+                tmpLdapDn = ldap_get_dn(ld, pEntry)
+				kprintf(L"[ldap] %s : ", tmpLdapDn);
+                ldap_memfree(tmpLdapDn);
 				pId = ldap_get_values_len(ld, pEntry, myAttrs[0]);
 				if(pId && pId[0])
 				{
@@ -1245,6 +1248,7 @@ BOOL kuhl_m_lsadump_dcshadow_build_convert_account_to_dn(PLDAP ld, PWSTR szDomai
 					status = TRUE;
 			}
 			ldap_msgfree(pMessage);
+            ldap_memfreeW(szDN);
 		}
 		LocalFree(szFilter);
 	}
@@ -1578,6 +1582,7 @@ NTSTATUS kuhl_m_lsadump_dcshadow_lingering_propagate(PDCSHADOW_DOMAIN_INFO info,
 		{
 			szNTDSADn = ldap_get_dn(info->ld, pEntry);
 			szServerDN = wcsstr(szNTDSADn, L",CN=") + 1;
+            ldap_memfree(szNTDSADn);
 			dwErr = ldap_search_s(info->ld, szServerDN, LDAP_SCOPE_BASE, NULL, szAttributes, FALSE, &pServerMessage);
 			if(dwErr == LDAP_SUCCESS)
 			{
