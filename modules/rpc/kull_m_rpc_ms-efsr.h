d7d33c849ff3d9f58d5b83e4edda8064a7feb64b
Alex Romero <ntalexio2@gmail.com>
2023-03-23 04:23:14 -0400

    add StubEfsRpc* functions to use desired interface

--- modules/rpc/kull_m_rpc_ms-efsr.h
+++ modules/rpc/kull_m_rpc_ms-efsr.h
@@ -23,7 +23,14 @@ long EfsRpcWriteFileRaw(PEXIMPORT_CONTEXT_HANDLE hContext, EFS_EXIM_PIPE* EfsInP
 void EfsRpcCloseRaw(PEXIMPORT_CONTEXT_HANDLE* hContext);
 long EfsRpcEncryptFileSrv(handle_t binding_h, wchar_t* FileName);
 long EfsRpcDecryptFileSrv(handle_t binding_h, wchar_t* FileName, unsigned long OpenFlag);
+long StubEfsRpcOpenFileRaw(PMIDL_STUB_DESC stub, handle_t binding_h, PEXIMPORT_CONTEXT_HANDLE* hContext, wchar_t* FileName, long Flags);
+long StubEfsRpcReadFileRaw(PMIDL_STUB_DESC stub, PEXIMPORT_CONTEXT_HANDLE hContext, EFS_EXIM_PIPE* EfsOutPipe);
+long StubEfsRpcWriteFileRaw(PMIDL_STUB_DESC stub, PEXIMPORT_CONTEXT_HANDLE hContext, EFS_EXIM_PIPE* EfsInPipe);
+void StubEfsRpcCloseRaw(PMIDL_STUB_DESC stub, PEXIMPORT_CONTEXT_HANDLE* hContext);
+long StubEfsRpcEncryptFileSrv(PMIDL_STUB_DESC stub, handle_t binding_h, wchar_t* FileName);
+long StubEfsRpcDecryptFileSrv(PMIDL_STUB_DESC stub, handle_t binding_h, wchar_t* FileName, unsigned long OpenFlag);
+
+PMIDL_STUB_DESC GetLsaRpcStub();
+PMIDL_STUB_DESC GetEfsRpcStub();
 
 RPC_IF_HANDLE efsrpc_v1_0_c_ifspec;
-
-extern void switch_to_lsarpc_endpoint();
